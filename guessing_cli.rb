877d5138b20f5b4317515f2a435670da5e547e74
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 11:23:19 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_response = gets.chomp
-  random_number = 1 + rand(6)
+  random_number = rand(6)
   # while user_response != "exit" || user_response 
   # end 
   if user_response == random_number
