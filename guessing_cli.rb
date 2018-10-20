bb6d8bb696aaddd77e85e77e78c2db1fa5b495d9
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 11:31:51 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,7 +5,6 @@ def run_guessing_game
   random_number = rand(1..6)
   # while user_response != "exit" || user_response 
   # end 
-  puts "#{user_response == random_number}"
   if user_response == random_number
     puts "You guessed the correct number!"
   else 
