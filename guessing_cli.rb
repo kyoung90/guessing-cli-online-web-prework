f9ae0969361cfa4686d99ac707a9908580171d11
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 11:21:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   if user_response == random_number
     puts "You guessed the correct number!"
   else 
-    puts "The computer guessed #{random_number}"
+    puts "The computer guessed #{random_number}."
   end 
   
   puts "Goodbye!"
